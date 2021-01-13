e53b30a682e971202e60655847bcf245e947f52f
evan <1137299673@qq.com>
2021-01-13 23:41:59 +0800

    🐐 feat: + mad tag

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="home"></div>
-
+  <mad-tag-demo></mad-tag-demo>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue'
+import MadTagDemo from '@/views/MadTagDemo.vue'
 
 @Options({
   components: {
     HelloWorld,
-
+    MadTagDemo,
   },
 })
-export default class Home extends Vue {
-
-}
+export default class Home extends Vue {}
 </script>
