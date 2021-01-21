0fb8df7ac573861086c9aa3d9fa5be2a995348cf
evan <1137299673@qq.com>
2021-01-21 17:35:54 +0800

    🎡 feat: add form textarea

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home"></div>
-  <mad-input-add-on-demo />
+  <mad-text-area-demo />
+  <mad-input-demo />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue'
+import MadTextAreaDemo from '@/views/MadTextAreaDemo.vue'
 import MadInputDemo from '@/views/MadInputDemo.vue'
-import MadInputAddOnDemo from '@/views/MadInputAddOnDemo.vue'
 
 @Options({
   components: {
     HelloWorld,
-    MadInputAddOnDemo,
+    MadTextAreaDemo,
+    MadInputDemo,
   },
 })
 export default class Home extends Vue {}
