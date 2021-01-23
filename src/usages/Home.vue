af5432156804a6326e7ae905c27280aef1c61fcd
evan <1137299673@qq.com>
2021-01-23 23:29:22 +0800

    feat: add pattern border composition

--- src/usages/Home.vue
+++ src/usages/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home"></div>
-  <mad-reveal-image-demo />
+  <mad-border-composition-demo />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue'
-import MadLineNoiseDemo from '@/usages/patterns/MadLineNoiseDemo.vue'
-import MadRevealImageDemo from '@/usages/patterns/MadRevealImageDemo.vue'
+import MadBorderCompositionDemo from '@/usages/patterns/MadBorderCompositionDemo.vue'
 
 @Options({
   components: {
     HelloWorld,
-    MadRevealImageDemo,
+    MadBorderCompositionDemo,
   },
 })
 export default class Home extends Vue {}
---
+++ src/usages/patterns/MadBorderCompositionDemo.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>basic</h2>
+  <mad-border-composition>
+    <div class="demo">hover to see effect</div>
+  </mad-border-composition>
+
+  <h2>configurable</h2>
+  <mad-border-composition border-color="pink" border-width="2px">
+    <div class="demo">hover to see effect</div>
+  </mad-border-composition>
+</template>
+
+<script>
+import MadBorderComposition from '@/patterns/MadBorderComposition'
+
+export default {
+  name: 'MadBorderCompositionDemo',
+  components: {
+    MadBorderComposition,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.demo {
+  width: 300px;
+  height: 150px;
+  background-color: white;
+  border: 1px solid black;
+}
+</style>
