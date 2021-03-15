28faa792937733e9ae62d1170bcc98c3fc4b473d
evan <1137299673@qq.com>
2021-03-15 16:01:10 +0800

    feat(madexpansion): implement expansion for FAQ usage

---
+++ src/usages/components/expansion/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <mad-doc-page>
+    <mad-code-block title="basic" :code="MadExpansionUsageBasicCode">
+      <mad-expansion-usage-basic></mad-expansion-usage-basic>
+    </mad-code-block>
+  </mad-doc-page>
+</template>
+
+<script>
+import MadDocPage from '@/document/MadDocPage'
+import MadCodeBlock from '@/document/MadCodeBlock'
+
+import MadExpansionUsageBasic from '@/usages/components/expansion/MadExpansionUsageBasic'
+import MadExpansionUsageBasicCode from '!raw-loader!@/usages/components/expansion/MadExpansionUsageBasic'
+
+export default {
+  name: 'IndexTemplate',
+  components: {
+    MadCodeBlock,
+    MadDocPage,
+    MadExpansionUsageBasic,
+  },
+  data() {
+    return {
+      MadExpansionUsageBasicCode,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
